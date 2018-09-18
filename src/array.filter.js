a3d53bd46538b770dd35a91d4d75580d36562fc5
Katrina Agustin <katrina.agustin@gmail.com>
2018-09-18 13:16:45 -0700

    completed filter exercise

--- src/array.filter.js
+++ src/array.filter.js
@@ -1,34 +1,65 @@
-const { notDivisibleBy, scoreBelow,  startsWith} = require('./functions-return-functions');
+  const { notDivisibleBy, scoreBelow,  startsWith} = require('./functions-return-functions');
+
+// function isOdd(value) {
+//   return value % 2 === 1
+// }
 
 function onlyOdds(arr){
   const result = []
-  for(const value of arr){
-    if(value % 2 === 1){
-      result.push(value);
-    }
-  }
+  return arr.filter(value => value % 2 === 1)
+  // return arr.filter(isOdd)
+  // return filter(arr, isOdd)
+  // for(const value of arr){
+  //   if(isOdd(value)){
+  //     result.push(value);
+  //   }
+  // }
   return result;
 }
+
+// function points(value) {
+//   return value.careerPoints < 30000
+// }
 
 function hasBelow30000CareerPoints(arr){
   const result = []
-  for(const value of arr){
-    if(value.careerPoints < 30000){
-      result.push(value);
-    }
-  }
+  return arr.filter(value => value.careerPoints < 30000)
+  // return arr.filter(points)
+  // return filter(arr, points)
+  // for(const value of arr){
+  //   if(points(value)){
+  //     result.push(value);
+  //   }
+  // }
   return result;
 }
 
+// function names(str) {
+//   return str[0] === 'A'
+// }
+
 function firstNameStartsWithA(arr){
   const result = []
-  for(const value of arr){
-    if(value[0] === 'A'){
-      result.push(value);
-    }
-  }
+  return arr.filter(str => str[0] === 'A')
+  // return arr.filter(names)
+  // return filter(arr, names)
+  // for(const value of arr){
+  //   if(names(value)){
+  //     result.push(value);
+  //   }
+  // }
   return result;
 }
+
+// function filter(arr, fn) {
+//   const result = [];
+//   for (const value of arr) {
+//     if (fn(value)) {
+//       result.push(value)
+//     }
+//   }
+//   return result;
+// }
 
 module.exports = {
   onlyOdds,
