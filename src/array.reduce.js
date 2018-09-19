13eb9bd2819d979d9e86f6265a3eb5960d579d41
Katrina Agustin <katrina.agustin@gmail.com>
2018-09-19 11:28:39 -0700

    abstracted and reduced

--- src/array.reduce.js
+++ src/array.reduce.js
@@ -1,39 +1,65 @@
-function sumNumbers(arr){
-  let result = 0
-  for(const value of arr){
-    result = result + value
-  }
+function add(a, b){
+  return a + b
+}
 
-  return result
+function sumNumbers(arr){
+  return arr.reduce(add, 0)
+  // return reduce(arr, add, 0);
+  // let result = 0
+  // for(const value of arr){
+  //   result = add(result, value)
+  // }
+  // return result
+}
+
+function count(acc, ele) {
+  if (acc.hasOwnProperty(ele)) {
+    acc[ele] = acc[ele] + 1
+  }
+  else {
+    acc[ele] = 1
+  }
+  return acc
 }
 
 function countLetters(str){
-  let result = {}
-  for(const value of str){
-    if(result.hasOwnProperty(value)){
-      result[value] = result[value] + 1
-    }
-    else{
-      result[value] = 1
-    }
+  const arr = str.split('')
+  return arr.reduce(count, {})
+  // return reduce(str, count, {})
+  // let result = {}
+  // for(const value of str){
+  //   result = count(result, value)
+  // }
+  // return result
+}
+
+function groupBy(acc, ele) {
+  if (acc.hasOwnProperty(ele.university)) {
+    acc[ele.university].push(ele)
   }
-  return result
+  else {
+    acc[ele.university] = [ele]
+  }
+  return acc
 }
 
 function groupByUniversity(arr){
-  let result = {}
-  for(const value of arr){
-    if(result.hasOwnProperty(value.university)){
-      result[value.university].push(value)
-    }
-    else {
-      result[value.university] = [ value ]
-    }
-  }
-
-  return result
+  return arr.reduce(groupBy, {})
+  // return reduce(arr, groupBy, {})
+  // let result = {}
+  // for(const value of arr){
+  //   result = groupBy(result, value)
+  // }
+  // return result
 }
 
+function reduce(passedIn, fn, initVal) {
+  let result = initVal;
+  for (const value of passedIn) {
+    result = fn(result, value)
+  }
+  return result
+}
 
 module.exports = {
   sumNumbers,
